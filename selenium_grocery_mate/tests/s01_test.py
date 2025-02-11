606492bec19072e391fa9f8ede859a653ebcc5db
IgorSilvares <igorsilvares@gmail.com>
2025-02-11 13:27:32 +0100

    Update s01_test.py

--- selenium_grocery_mate/tests/s01_test.py
+++ selenium_grocery_mate/tests/s01_test.py
@@ -6,53 +6,59 @@ from pages.orange_page import OrangePage
 from pages.shop_page import ShopPage
 from utils.constants import user, password
 
-def test_five_star(driver):
-    homepage = LoginPage(driver).open_page_and_login(user, password)
+def test_three_star(driver):
+    try:
+        # Open the login page and log in
+        homepage = LoginPage(driver).open_page_and_login(user, password)
 
-    # Wait for the homepage to be loaded
-    WebDriverWait(driver, 3).until(
-        EC.url_to_be('https://grocerymate.masterschool.com/')
-    )
+        # Wait for the homepage to be loaded
+        WebDriverWait(driver, 3).until(
+            EC.url_to_be('https://grocerymate.masterschool.com/')
+        )
 
-    homepage.click_shop()
+        # Click the shop button
+        homepage.click_shop()
 
-    # Wait for the shop page to be loaded
-    WebDriverWait(driver, 3).until(
-        EC.url_to_be('https://grocerymate.masterschool.com/store')
-    )
+        # Wait for the shop page to be loaded
+        WebDriverWait(driver, 3).until(
+            EC.url_to_be('https://grocerymate.masterschool.com/store')
+        )
 
-    shop_page = ShopPage(driver)
-    shop_page.click_age_confirm_button()
+        # Click the age confirm button
+        shop_page = ShopPage(driver)
+        shop_page.click_age_confirm_button()
 
-    # Wait for the orange element to be present
-    orange_element = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located(shop_page.orange_locator)
-    )
+        # Wait for the orange element to be present
+        orange_element = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(shop_page.orange_locator)
+        )
 
-    shop_page.click_orange()
+        # Click the orange element
+        shop_page.click_orange()
+        orange_page = OrangePage(driver)
 
-    orange_page = OrangePage(driver)
+        # Wait for the three-star button to be present
+        five_star_button = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(orange_page.three_star_locator)
+        )
 
-    # Wait for the five-star button to be present
-    five_star_button = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located(orange_page.five_star_locator)
-    )
 
-    orange_page.click_five_star_button()
-    orange_page.click_send_button()
+        # Click the three-star button
+        orange_page.click_three_star_button()
+        orange_page.click_send_button()
 
-    # Wait for the orange review to be present
-    orange_review = WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located(orange_page.review_div_locator)
-    )
+        # Wait for the orange review to be present
+        orange_review = WebDriverWait(driver, 3).until(
+            EC.presence_of_element_located(orange_page.review_div_locator)
+        )
 
-    # Get the review rating
-    user_rating = orange_page.driver.find_element(*orange_page.review_user_rate).text
+        # Get the review rating
+        user_rating = orange_page.driver.find_element(*orange_page.review_user_rate).text
 
-    # Assert that the user rating is 5
-    assert user_rating == '(5)'
+        # Assert that the user rating is 3 and the user feedback is correct
+        assert user_rating == '(3)'
 
-    # Delete the review
-    orange_page.delete_review()
-
-    driver.quit()
+    finally:
+        # Delete the review
+        orange_page = OrangePage(driver)
+        orange_page.delete_review()
