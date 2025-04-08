385bc99a1f1354197d8c7ab232dbe00dba02d4f2
IgorSilvares <104534919+IgorSilvares@users.noreply.github.com>
2025-04-09 00:23:11 +0200

    Create age_test.py

---
+++ selenium_grocery_mate/tests/age_test.py
@@ -0,0 +1,32 @@
+from pages.login_page import LoginPage
+from utils.constants import user, password
+
+
+def _age_test_helper(driver, birth_date, valid_age):
+    """Helper function for age verification tests"""
+
+    # Login with user and password
+    homepage = LoginPage(driver).open_page_and_login(user, password)
+
+    # Enter the Shop Page
+    shop_page = homepage.click_shop()
+
+    # Enter the birthdate
+    shop_page.enter_age(birth_date)
+
+    # Click the confirm button
+    shop_page.click_age_confirm_button()
+    alcohol_page = shop_page.click_alcohol()
+
+    if valid_age:
+        assert not alcohol_page.is_underage_message_visible()
+    else:
+        assert alcohol_page.is_underage_message_visible()
+
+
+def test_valid_age(driver):
+    _age_test_helper(driver, "01-01-2000", valid_age=True)
+
+
+def test_invalid_age(driver):
+    _age_test_helper(driver, "01-01-2020", valid_age=False)
