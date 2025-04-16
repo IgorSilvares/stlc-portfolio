66f1a9649fafc012d1bb953fc1c47a6f5f1fb2cc
IgorSilvares <104534919+IgorSilvares@users.noreply.github.com>
2025-04-16 12:20:39 +0200

    Update home_page.py

--- selenium_grocery_mate/pages/home_page.py
+++ selenium_grocery_mate/pages/home_page.py
@@ -1,7 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from pages.shop_page import ShopPage
 from .checkout_page import CheckoutPage
 
@@ -19,17 +16,8 @@ class HomePage:
     def click_shop(self):
         # Click the shop button
         self.driver.find_element(*self.shop_locator).click()
-
-        # Initialize ShopPage first to access its locators
-        shop_page = ShopPage(self.driver)
-
-        # Wait for a shop page element to be present
-        WebDriverWait(self.driver, 3).until(
-            EC.presence_of_element_located(shop_page.search_input_locator)
-        )
-
-        return shop_page
+        return ShopPage(self.driver)
 
     def click_checkout(self):
         self.driver.find_element(*self.checkout_locator).click()
-        return CheckoutPage()
+        return CheckoutPage(self.driver)
