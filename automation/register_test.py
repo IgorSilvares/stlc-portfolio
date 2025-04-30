159b603613fc30aee9e075e66377b953224b02f2
IgorSilvares <104534919+IgorSilvares@users.noreply.github.com>
2025-04-30 13:25:41 +0200

    Update register_test.py

--- automation/register_test.py
+++ automation/register_test.py
@@ -13,13 +13,38 @@ def web_driver():
     driver.quit()
 
 
+@pytest.fixture
+def user_registration_data():
+    return {
+        "name": "user",
+        "email": "user998@user.com",
+        "password": "password123",
+        "title": "Mr",
+        "birth_date": {
+            "day": "5",
+            "month": "March",
+            "year": "1991"
+        },
+        "first_name": "User",
+        "last_name": "Resu",
+        "company": "User Company",
+        "address1": "Street",
+        "address2": "City",
+        "country": "Canada",
+        "state": "State",
+        "city": "City",
+        "zipcode": "123123123",
+        "mobile_number": "99999999999"
+    }
+
+
 def test_if_page_is_displayed(web_driver):
     driver = web_driver
     page_logo = driver.find_element(By.XPATH, '//img[@src="/static/images/home/logo.png"]')
     assert page_logo.is_displayed()
 
 
-def test_new_user_register(web_driver):
+def test_new_user_register(web_driver, user_registration_data):
     driver = web_driver
 
     # Close cookies pop up
@@ -38,8 +63,8 @@ def test_new_user_register(web_driver):
     # Enter new Name and new Email and click signup
     name_input = driver.find_element(By.NAME, "name")
     email_input = driver.find_element(By.XPATH, '//input[@data-qa="signup-email"]')
-    name_input.send_keys("user")
-    email_input.send_keys("user998@user.com")
+    name_input.send_keys(user_registration_data["name"])
+    email_input.send_keys(user_registration_data["email"])
     signup_button = driver.find_element(By.XPATH, '//button[@data-qa="signup-button"]')
     signup_button.click()
 
@@ -48,21 +73,29 @@ def test_new_user_register(web_driver):
     assert enter_account_text.is_displayed()
 
     # Fill details: Title, Name, Email, Password, Date of birth
-    mr_title_button = driver.find_element(By.ID, 'id_gender1')
-    mr_title_button.click()
+    if user_registration_data["title"] == "Mr":
+        mr_title_button = driver.find_element(By.ID, 'id_gender1')
+        mr_title_button.click()
+    else:
+        mrs_title_button = driver.find_element(By.ID, 'id_gender2')
+        mrs_title_button.click()
+        
     password_input = driver.find_element(By.ID, 'password')
-    password_input.send_keys("password123")
+    password_input.send_keys(user_registration_data["password"])
+    
     day_selector = driver.find_element(By.ID, 'days')
     day_selector.click()
-    day_option = driver.find_element(By.XPATH, "//option[text()='5']")
+    day_option = driver.find_element(By.XPATH, f"//option[text()='{user_registration_data['birth_date']['day']}']")
     day_option.click()
+    
     month_selector = driver.find_element(By.ID, 'months')
     month_selector.click()
-    month_option = driver.find_element(By.XPATH, "//option[text()='March']")
+    month_option = driver.find_element(By.XPATH, f"//option[text()='{user_registration_data['birth_date']['month']}']")
     month_option.click()
+    
     year_selector = driver.find_element(By.ID, 'years')
     year_selector.click()
-    year_option = driver.find_element(By.XPATH, "//option[text()='1991']")
+    year_option = driver.find_element(By.XPATH, f"//option[text()='{user_registration_data['birth_date']['year']}']")
     year_option.click()
 
     # Select checkbox 'Sign up for our newsletter!'
@@ -75,27 +108,36 @@ def test_new_user_register(web_driver):
 
     # Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
     first_name_input = driver.find_element(By.ID, 'first_name')
-    first_name_input.send_keys("User")
+    first_name_input.send_keys(user_registration_data["first_name"])
+    
     last_name_input = driver.find_element(By.ID, 'last_name')
-    last_name_input.send_keys("Resu")
+    last_name_input.send_keys(user_registration_data["last_name"])
+    
     company_input = driver.find_element(By.ID, 'company')
-    company_input.send_keys("User Company")
+    company_input.send_keys(user_registration_data["company"])
+    
     address_input = driver.find_element(By.ID, 'address1')
-    address_input.send_keys("Street")
+    address_input.send_keys(user_registration_data["address1"])
+    
     address2_input = driver.find_element(By.ID, 'address2')
-    address2_input.send_keys("City")
+    address2_input.send_keys(user_registration_data["address2"])
+    
     country_selector = driver.find_element(By.ID, 'country')
     country_selector.click()
-    country_option = driver.find_element(By.XPATH, '//option[text()="Canada"]')
+    country_option = driver.find_element(By.XPATH, f'//option[text()="{user_registration_data["country"]}"]')
     country_option.click()
+    
     state_input = driver.find_element(By.ID, 'state')
-    state_input.send_keys("State")
+    state_input.send_keys(user_registration_data["state"])
+    
     city_input = driver.find_element(By.ID, 'city')
-    city_input.send_keys("City")
+    city_input.send_keys(user_registration_data["city"])
+    
     zipcode_input = driver.find_element(By.ID, 'zipcode')
-    zipcode_input.send_keys("123123123")
+    zipcode_input.send_keys(user_registration_data["zipcode"])
+    
     mobile_number_input = driver.find_element(By.ID, 'mobile_number')
-    mobile_number_input.send_keys("99999999999")
+    mobile_number_input.send_keys(user_registration_data["mobile_number"])
 
     # Click 'Create Account button'
     create_account_button = driver.find_element(By.XPATH, '//button[@data-qa="create-account"]')
@@ -115,7 +157,9 @@ def test_new_user_register(web_driver):
 
     # Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
     account_deleted_text = driver.find_element(By.XPATH, '//b[text()="Account Deleted!"]')
+    assert account_deleted_text.is_displayed()
 
 
 if __name__ == "__main__":
     pytest.main()
+    
